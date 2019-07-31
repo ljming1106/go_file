7894822529d9fc262bdf5d303f04adc8f21bce10
Jimmy <ljming1106@163.com>
2019-07-31 18:39:00 +0800

    modify code to interface

--- file_server.go
+++ file_server.go
@@ -16,38 +16,18 @@ const Udp = "udp"
 
 var waitGroup = sync.WaitGroup{}
 
-func main() {
-	/*三种访问方式：
-	 1、http
-	  1）创建goroutine，监听端口
-	  2）当有连接进来时，新建goroutine处理内容
-
-	 2、tcp
- 	  1）创建goroutine，监听端口
-	  2）当有连接进来时，处理内容
-
-	 3、udp
-	tcp和udp的实现
-	https://www.jianshu.com/p/dec62eff73ba
-	*/
-	accessWay := [...]string{Http, Tcp, Udp}
-	for _, Type := range accessWay {
-		switch Type {
-		case Http:
-			go httpAccess()
-		case Tcp:
-			go tcpAccess()
-		case Udp:
-			go udpAccess()
-		}
-	}
-	//设置信号量，等待返回goroutine返回才结束
-	waitGroup.Add(2)
-	waitGroup.Wait()
-	fmt.Println("所有groutine已经退出")
+type netAccess interface {
+	access()
+	handleConn(interface{})
 }
 
-func httpAccess() {
+type httpWay string
+type tcpWay string
+type udpWay string
+
+//三种类型实现netAccess的所有方法
+/*HTTP*/
+func (httpDo httpWay) access() {
 	for {
 		httpHandler := func(w http.ResponseWriter, req *http.Request) {
 			res := dealReadData()
@@ -57,8 +37,11 @@ func httpAccess() {
 		log.Println(http.ListenAndServe(":8000", nil).Error())
 	}
 }
+func (httpDo httpWay) handleConn(interface{}) {
+}
 
-func tcpAccess() {
+/*TCP*/
+func (tcpDo tcpWay) access() {
 	listener, err := net.Listen("tcp", "localhost:8001")
 	defer deferTcpDeal(listener)
 	if err != nil {
@@ -73,21 +56,71 @@ func tcpAccess() {
 			continue
 		}
 
-		go handleTcpConn(conn)
+		go tcpDo.handleConn(conn)
 	}
 }
+func (tcpDo tcpWay) handleConn(conn interface{}) {
+	var c net.Conn = conn.(net.Conn)
+	defer c.Close()
+	res := dealReadData()
+	io.WriteString(c, res)
+}
 
-func udpAccess() {
+/*UDP*/
+func (udpDo udpWay) access() {
 	defer waitGroup.Done()
 	for {
 		conn, err := net.ListenPacket("udp", "localhost:8002")
 		//go handleUdpConn(conn)
-		handleUdpConn(conn)
+		udpDo.handleConn(conn)
 		if err != nil {
 			log.Printf(err.Error())
 		}
 
 	}
+}
+func (udpDo udpWay) handleConn(conn interface{}) {
+	var c net.PacketConn = conn.(net.PacketConn)
+	buffer := make([]byte, 1024)
+	_, remoteAddr, err := c.ReadFrom(buffer)
+	if err != nil {
+		log.Panicln("ReadFrom err", err.Error())
+	}
+	go handleUdpConn1(c, remoteAddr)
+}
+
+func handleUdpConn1(conn net.PacketConn, remoteAddr net.Addr) {
+	defer deferUdpDeal(conn)
+	res := dealReadData()
+	conn.WriteTo([]byte(res), remoteAddr)
+}
+
+/*三种访问方式：
+ 1、http
+  1）创建goroutine，监听端口
+  2）当有连接进来时，新建goroutine处理内容
+
+ 2、tcp
+   1）创建goroutine，监听端口
+  2）当有连接进来时，处理内容
+
+ 3、udp
+	tcp和udp的实现
+	https://www.jianshu.com/p/dec62eff73ba
+*/
+func main() {
+	var httpDo httpWay = Http
+	var tcpDo tcpWay = Tcp
+	var udpDo udpWay = Udp
+
+	accessWay := [...]netAccess{httpDo, tcpDo, udpDo}
+	for _, Type := range accessWay {
+		  go Type.access()
+		}
+	//设置信号量，等待返回goroutine返回才结束
+	waitGroup.Add(2)
+	waitGroup.Wait()
+	fmt.Println("所有groutine已经退出")
 }
 
 func deferTcpDeal(c net.Listener) {
@@ -97,27 +130,6 @@ func deferTcpDeal(c net.Listener) {
 
 func deferUdpDeal(c net.PacketConn) {
 	c.Close()
-}
-
-func handleTcpConn(c net.Conn) {
-	defer c.Close()
-	res := dealReadData()
-	io.WriteString(c, res)
-}
-
-func handleUdpConn(conn net.PacketConn) {
-	buffer := make([]byte, 1024)
-	_, remoteAddr, err := conn.ReadFrom(buffer)
-	if err != nil {
-		log.Panicln("ReadFrom err", err.Error())
-	}
-	go handleUdpConn1(conn, remoteAddr)
-}
-
-func handleUdpConn1(conn net.PacketConn,remoteAddr net.Addr) {
-	defer deferUdpDeal(conn)
-	res := dealReadData()
-	conn.WriteTo([]byte(res), remoteAddr)
 }
 
 func dealReadData() string {
